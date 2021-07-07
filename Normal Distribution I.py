ca3315f0fe4b548199dede6bbde8bab99a3e87f3
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2021-07-07 11:11:31 +0530

    Update Normal Distribution I.py

--- Normal Distribution I.py
+++ Normal Distribution I.py
@@ -1,8 +1,23 @@
 import math
-mean, std = 70, 10
-cdf = lambda x: 0.5 * (1 + math.erf((x - mean) / (std * (2 ** 0.5))))
+mean=20.0
+stddev=2.0
+h1=19.5
+h21,h22=20.0,22.0
 
+def integrate(func,b,n=10000):
+    step=1/n
+    if(b<0):step=-step
+    n=int(abs(b)*n)
+    a=0.0
+    sq=0.0
+    for _ in range(0,n):
+        sq+=step*func(a)
+        a+=step
+    return abs(sq)
 
-print(round((1-cdf(80))*100,2))
-print(round((1-cdf(60))*100,2))
-print(round((cdf(60))*100,2))
+erf = lambda b : (2*math.pi**(-0.5)) * integrate(lambda x: math.exp(-x**2),b)
+phi = lambda b : (1 + erf( (b-mean) / (stddev * 2**0.5) ))/2
+lesh1 = phi(0.0) - phi(h1)
+beth1h2 = phi(h22) - phi(h21)
+print(f'{lesh1:.3f}')
+print(f'{beth1h2:.3f}')
